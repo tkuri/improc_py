ba2b8e4c6837eb0cd79fb03f7f3077796451227c
tkuri <kuritateppei@gmail.com>
2019-08-05 13:06:20 +0900

    Added makeplate project.

---
+++ makeplate/makeplate.py
@@ -0,0 +1,11 @@
+import numpy as np
+import cv2
+
+if __name__ == '__main__':
+    buf = np.zeros((128, 128, 3),  dtype=np.int)
+
+    for i in range(0, 256):
+        buf[:] = i
+        ofname = 'plate' + str(i).zfill(3) + '.png'
+        cv2.imwrite(ofname, buf)
+
