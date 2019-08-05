92946d768dd648cf5bc150f1b19cc5256cba90ee
tkuri <kuritateppei@gmail.com>
2019-08-05 13:05:55 +0900

    Modified default type.

--- template/template.py
+++ template/template.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 
 if __name__ == '__main__':
-    buf = np.zeros((128, 128),  dtype=np.int)
-    
+    buf = np.zeros((128, 128, 3),  dtype=np.int)
+
     cv2.imwrite('test.png', buf)
 
