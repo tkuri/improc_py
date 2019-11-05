d493bdae0a80eaec22bc3195ed3b02a0d52d9ba2
tkuri <kuritateppei@gmail.com>
2019-11-05 13:40:58 +0900

    Modified default param.

--- makeplate/makeplate.py
+++ makeplate/makeplate.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 
 if __name__ == '__main__':
-    buf = np.zeros((128, 128, 3),  dtype=np.int)
+    buf = np.zeros((192, 192, 3),  dtype=np.int)
 
     for i in range(0, 256):
         buf[:] = i
